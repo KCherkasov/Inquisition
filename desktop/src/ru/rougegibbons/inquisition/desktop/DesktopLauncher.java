a5ca30ba729600dbee67e2d5f8ebb60208a3e918
Kirill Cherkasov <kvcherk@live.ru>
2021-05-03 19:10:58 +0300

    Initial commit

---
+++ desktop/src/ru/rougegibbons/inquisition/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.rougegibbons.inquisition.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.rougegibbons.inquisition.InquisitorGame;
+
+public class DesktopLauncher {
+	public static void main(String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new InquisitorGame(), config);
+	}
+}
